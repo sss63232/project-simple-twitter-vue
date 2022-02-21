dc0bc20ad4db95a88e0e45c08c8811f91e606473
a2623212@gmail.com <A5GjZK7w>
2022-02-22 00:04:11 +0800

    feat: add NotFound.vue and set the route

---
+++ src/views/NotFound.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="not-found container">
+    <h1>404 Page Not Found</h1>
+    <img src="../assets/404.png" alt="404" class="img" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.not-found {
+  text-align: center;
+  h1 {
+    color: black;
+    margin-bottom: 10px;
+  }
+  .img {
+    margin: 0 auto;
+    border-radius: 20px;
+    width: 500px;
+  }
+}
+</style>
