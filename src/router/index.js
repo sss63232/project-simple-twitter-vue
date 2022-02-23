e9468471c41ad3174efe55523235205eb023d6c6
content Tingching0602 <contentanita2131love@gmail.com>
2022-02-23 15:13:11 +0800

    v1

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,41 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import NotFound from '@/views/NotFound.vue';
+import register from '../views/register.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
+  // {
+  //   path: '/',
+  //   name: 'root',
+  //   redirect: '/login'
+  // },
   {
     path: '*',
     name: 'not-found',
     component: NotFound,
   },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/login.vue'),
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: register,
+  },
+//   {
+//     path: '/admin',
+//     name: 'admin-root',
+//     redirect: '/admin/signin',
+//   },
+//   {
+//     path: '/admin/signin',
+//     name: 'admin-signin',
+//     component: admin,
+//   },
 ];
 
 const router = new VueRouter({
