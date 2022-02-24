34c7f6a99efbb2ac10f72ab636d03cefb982580a
a2623212@gmail.com <A5GjZK7w>
2022-02-24 13:12:27 +0800

    feat: 切換 tabs 切版

--- src/components/ProfileTabs.vue
+++ src/components/ProfileTabs.vue
@@ -1,14 +1,27 @@
 <template>
   <div class="main">
     <ul class="tabs">
-      <li class="tabs__posts">推文</li>
-      <li class="tabs__replies">推文與回覆</li>
-      <li class="tabs__likes">喜歡的內容</li>
+      <router-link to="/user/:id/tweets">
+        <li class="tabs__posts active">
+          推文
+        </li>
+      </router-link>
+       <router-link to="/user/:id/replies">
+        <li class="tabs__replies">
+          推文與回覆
+        </li>
+      </router-link>
+      <router-link to="/user/:id/likes">
+        <li class="tabs__likes">
+          喜歡的內容
+        </li>
+      </router-link>
     </ul>
   </div>
 </template>
 
 <style lang="scss" scoped>
+@import './../styles/variables.scss' ;
 .main {
   width: 100%;
   margin-top:1rem;
@@ -16,7 +29,19 @@
     display: flex;
     li {
       width: 130px;
+      height: 3rem;
+      text-align: center;
+      line-height: 3rem;
+      font: {
+        size:15px;
+        weight: 700;
+      }
+      color: $secondaryTextColor;
     }
+    .active{
+        border-bottom: 2px solid $orange;
+        color: $orange;
+      }
   }
 }
 </style>
