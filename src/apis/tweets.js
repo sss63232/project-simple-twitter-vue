6f6d01ac2ca34630db6e61a319d7f8c15ddd7442
Alvin <AlvinWang627@gmail.com>
2022-02-28 14:07:06 +0800

    api (get tweet/like/unlike function)

---
+++ src/apis/tweets.js
@@ -0,0 +1,30 @@
+import { apiHelper } from "../utils/helper";
+const getToken = () => localStorage.getItem('token')
+
+export default {
+  //main.vue首頁拿所有推文的api ok
+  getTweets() {
+    return apiHelper.get('/tweets',
+      {
+        headers: { Authorization: `Bearer ${getToken()}` }
+      })
+  },
+  //新增一則推文 failure 
+  createTweet({ tweet }) {
+    return apiHelper.post('/tweets', { tweet }, {
+      headers: { Authorization: `Bearer ${getToken()}` }
+    })
+  },
+  //推文可以加入like ok
+  addLike({ tweetId }) {
+    return apiHelper.post(`tweets/${tweetId}/like`, null, {
+      headers: { Authorization: `Bearer ${getToken()}` }
+    })
+  },
+  //推文可以移除like ok
+  deleteLike({ tweetId }) {
+    return apiHelper.post(`tweets/${tweetId}/unlike`, null, {
+      headers: { Authorization: `Bearer ${getToken()}` }
+    })
+  },
+}
--- src/components/tweet.vue
+++ src/components/tweet.vue
@@ -20,14 +20,30 @@
 <script>
 import { v4 as uuidv4 } from "uuid";
 
+const dummyUser = {
+  id: 14,
+  name: "user1",
+  email: "user1@example.com",
+  avatar: "https://loremflickr.com/140/140/people?random=100",
+  introduction:
+    "Sint amet reprehenderit et eligendi est harum. Quis facere placeat. Quia molestiae error optio dolor",
+  role: "",
+  account: "user1",
+  cover: "https://loremflickr.com/600/200/nature?random=100",
+  createdAt: "2022-02-26T03:59:35.000Z",
+  updatedAt: "2022-02-26T03:59:35.000Z",
+};
 export default {
   data() {
     return {
       text: "",
+      currentUser: dummyUser,
     };
   },
   methods: {
     handleSubmit() {
+      // const form = e.target;
+      // const formData = new FormData(form);
       if (this.text.length > 140) {
         return alert("字數超過140個");
       }
@@ -35,8 +51,16 @@ export default {
         return alert("不可空白");
       }
       this.$emit("after-create-tweet", {
+        // formData,
         tweetId: uuidv4(),
-        text: this.text,
+        UserId: this.currentUser.id,
+        name: this.currentUser.name,
+        image: this.currentUser.avatar,
+        account: this.currentUser.account,
+        description: this.text,
+        RepliesCount: 0,
+        LikesCount: 0,
+        createdAt: new Date(),
       });
       this.text = "";
     },
