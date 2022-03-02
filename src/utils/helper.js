415e73d9140f5711c0cd0f64cb45176bf5c9771f
Alvin <AlvinWang627@gmail.com>
2022-03-02 15:57:07 +0800

    feat: fix toast

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -8,21 +8,12 @@ export const apiHelper = axios.create({
   baseURL,
 });
 
-// export const Toast = Swal.mixin({
-//   toast: true,
-//   position: "top-end",
-//   showConfirmButton: false,
-//   timer: 3000,
-// })
-
-// export default Toast
-
 //成功用
 export const Toast = Swal.mixin({
   toast: true,
   position: 'top-end',
   showConfirmButton: false,
-  // timer: 3000,
+  timer: 3000,
   imageUrl: 'https://i.imgur.com/Mip3qeP.jpg',
 })
 export default Toast
@@ -31,7 +22,7 @@ export default Toast
 export const Toast2 = Swal.mixin({
   toast: true,
   position: 'top-end',
-  // timer: 3000,
+  timer: 3000,
   showConfirmButton: false,
   imageUrl: 'https://i.imgur.com/Bc63ZDR.png',
 })
